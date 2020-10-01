573f845d8465d80c8fc8ea964ee3d1d02e4ad7ab
António Azevedo <antonio.v.azevedo@gmail.com>
2020-10-01 12:29:45 +0100

    make package translatable

--- src/Http/Controllers/ToolController.php
+++ src/Http/Controllers/ToolController.php
@@ -9,20 +9,22 @@ use Davidpiesse\NovaMaintenanceMode\MaintenanceMode;
 
 class ToolController extends Controller
 {
-    public function up(Request $request){
+    public function up(Request $request)
+    {
         MaintenanceMode::up();
 
         return response([
-            'message' =>'Application is now live',
+            'message' =>__('Application is now live'),
             'currentlyInMaintenance' => app()->isDownForMaintenance(),
         ], 200);
     }
 
-    public function down(Request $request){
+    public function down(Request $request)
+    {
         MaintenanceMode::down($request);
 
         return response([
-            'message' =>'Application is now in maintenance mode',
+            'message' =>__('Application is now in maintenance mode'),
             'currentlyInMaintenance' => app()->isDownForMaintenance(),
         ], 200);
     }
